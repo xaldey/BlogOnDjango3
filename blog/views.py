9b3b908740ded7b91009e9880fe310875e96bfca
xaldey <fguzkp25@gmail.com>
2021-11-12 17:30:51 +0300

    Добавил форму для отправки сообщения

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -39,3 +40,19 @@ def post_detail(request, year, month, day, post):
                   'blog/post/detail.html',
                   {'post': post}
                   )
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='опубликован')
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
