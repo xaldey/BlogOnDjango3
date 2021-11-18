108cbd6bdeb2dda51347d332c6f3ccedcfd492b6
xaldey <fguzkp25@gmail.com>
2021-11-18 14:38:02 +0300

    Добавил sitemap на сайт

--- centr_web/settings.py
+++ centr_web/settings.py
@@ -26,6 +26,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
